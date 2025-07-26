900b7acf2da91a80a4061b0caf4c0a3337a0f60f
JeanLima2112 <jeanlima5939@gmail.com>
2025-07-26 20:25:11 -0300

    Fix: Toast

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import { BrowserRouter } from "react-router-dom";
 import { Router } from "./router";
 import { ThemeProvider } from "./contexts/theme";
 import { environment } from "./config";
+import { Toaster } from "./components/ui/sonner";
 
 export function App() {
   const theme: "light" | "dark" = environment.THEME;
   return (
     <BrowserRouter>
       <ThemeProvider defaultTheme={theme}>
+        <Toaster
+          position="bottom-right"
+          duration={5000}
+          theme={theme}
+          closeButton
+        />
         <Router />
       </ThemeProvider>
     </BrowserRouter>
