5ec83f08a0fa4b4194e3d4ef8d518eaf4f57baef
JeanLima2112 <jeanlima5939@gmail.com>
2025-07-26 19:37:52 -0300

    Feat: Auth Service

---
+++ src/services/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { api } from "@/config";
+
+export class AuthService {
+  public static async login(email: string, password: string): Promise<string> {
+    const { data } = await api.post<{ token: string }>("/auth/login", {
+      email,
+      password,
+    });
+
+    return data.token;
+  }
+}
